ad30bde86a2dc64a3e260c97340e3ca644e0f77d
Giovanni 'mac' Giorgi <jj@gioorgi.com>
2014-09-28 17:22:07 +0200

    first prototype of the eggbox

--- README.md
+++ README.md
@@ -9,6 +9,13 @@ Init the submodule THEN point your Arduino IDE Sketchbook location to something 
 <3dprinter_sensor>/src
 
 
+Work in progress Notes 
+===========================
 Aiko fbb132965599bc404244af1bd7fe517b5f0443c5
-worked years ago, now needs OneWire.h
+worked years ago, now needs OneWire.h (not included, not mentioned!!).
+
+
+PING: Eval also
+https://code.google.com/p/arduino-new-ping/
+which seems a lot better.
 
---
+++ src/the_4egg_box/the_4egg_box.ino
@@ -0,0 +1,118 @@
+#include <EventManager.h>
+#include <pitches_it.h>
+
+const int speakerOut=A5;
+
+// Boot Music
+void taDa(int event, int p){
+  tone(speakerOut,NOTE_SI4);
+  delay(100);
+  tone(speakerOut,NOTE_LA3);
+  delay(100);
+  tone(speakerOut,NOTE_DO2);
+  delay(100);
+  noTone(speakerOut);
+
+
+}
+
+#include <dht11.h>
+dht11 DHT;
+
+#define DHT11_PIN 4       // define a pin for DHT sensor
+
+enum BlinkCmd {
+  BlinkOn, BlinkOff
+};
+
+
+// No interrupts, so can use the non-interrupt safe mode
+EventManager gEM( EventManager::kNotInterruptSafe );
+
+void setup(){  
+ pinMode(13, OUTPUT);
+ Serial.begin(9600);
+ Serial.println("3D Printer OSNF - Open Sensors Network Framework 0.1 ");
+ Serial.print("DHT LIBRARY VERSION:");
+ Serial.println(DHT11LIB_VERSION);
+ Serial.print("SENSOR NAME:");
+ Serial.println("OSA1");
+ Serial.println();
+ gEM.addListener( EventManager::kEventUser0, taDa );
+ gEM.addListener( EventManager::kEventUser1, blinker );
+
+}
+
+unsigned long lastToggledBlinker;
+BlinkCmd lastBlinkCmd=BlinkOn;
+
+void blinker(int event, int cmd){
+  BlinkCmd command= (BlinkCmd)cmd;
+  switch (command) {
+    case BlinkOn:
+      digitalWrite(13, HIGH); 
+      break;
+    case BlinkOff:
+      digitalWrite(13, LOW); 
+      break;
+    default:
+       Serial.println("blinker error unkown command:"+cmd);
+  }
+  lastToggledBlinker=millis();
+}
+
+
+void temperatureListener(int event, int pin){
+  int chktemp =  DHT.read(pin); // DHT.read(DHT11_PIN);
+  switch (chktemp){
+    case DHTLIB_OK:  
+                //Serial.print("OK,\t"); 
+                {
+                  int temp = DHT.temperature; 
+                  int humidity = DHT.humidity;
+                  Serial.print("TEMP:");
+                  Serial.print(temp);
+                  Serial.print(",");
+                  Serial.print("HUM:");
+                  Serial.print(humidity);                 
+                }
+                // Retrofit an event                
+                break;
+    case DHTLIB_ERROR_CHECKSUM: 
+                Serial.print("Checksum error,\t"); 
+                break;
+    case DHTLIB_ERROR_TIMEOUT: 
+                Serial.print("Time out error,\t"); 
+                break;
+    default: 
+                Serial.print("Unknown error,\t"); 
+                break;
+  }
+}
+
+
+
+
+
+void loop() 
+{
+    // Handle any events that are in the queue
+    gEM.processEvent();
+    // Push a blinker and a sensor check
+    if ( ( millis() - lastToggledBlinker ) > 2000 ) 
+    {      
+      gEM.queueEvent( EventManager::kEventUser1, lastBlinkCmd);
+      if(lastBlinkCmd==BlinkOn) { 
+        lastBlinkCmd=BlinkOff;
+      }else{
+        lastBlinkCmd=BlinkOn;
+      }
+      gEM.queueEvent( EventManager::kEventUser0, DHT11_PIN);
+    }
+    // Push another event after 2000 sec
+    
+    delay(2000);
+       
+
+}
+
